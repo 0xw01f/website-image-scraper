a19e4afa7e92d3c658afe8f4c1853fcd23a3f13f
0xw01f <76235339+0xw01f@users.noreply.github.com>
2021-03-21 16:44:44 +0100

    first commit

---
+++ scraper.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import os
+
+URL = "https://w01f.xyz/"
+
+soup = bs(requests.get(URL).content, "html.parser")
+img_urls = []
+for img in soup.find_all("img"):
+    img_url = URL + img.attrs.get("src")
+    img_urls.append(img_url)
+
+for url in img_urls:
+    response = requests.get(url)
+    size = int(response.headers.get("Content-Length", 0))
+    name = os.path.join("images", url.split("/")[-1])
+    with open(name, "wb") as f_out:
+        f_out.write(response.content)
