10de421d3218b21c57e7754ba6d043255861fd4c
0xw01f <76235339+0xw01f@users.noreply.github.com>
2021-03-21 18:26:18 +0100

    create a folder with the name of the targetted website

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup as bs
+from urllib.parse import urlparse
 import requests
 import os
 
-URL = "https://w01f.xyz/"
+URL = "https://cyberocc.com/"
 
 soup = bs(requests.get(URL).content, "html.parser")
 img_urls = []
@@ -10,13 +11,19 @@ for img in soup.find_all("img"):
     img_url = URL + img.attrs.get("src")
     img_urls.append(img_url)
 
+image_path = 'images/' + urlparse(URL).netloc
 
-if not os.path.exists('images'):
-    os.makedirs('images')
+if not os.path.exists(image_path):
+    os.makedirs(image_path)
 
 for url in img_urls:
+    print(url.split("/")[-1])
     response = requests.get(url)
     size = int(response.headers.get("Content-Length", 0))
-    name = os.path.join("images", url.split("/")[-1])
-    with open(name, "wb") as f_out:
-        f_out.write(response.content)
+    try:
+        name = os.path.join(image_path, url.split("/")[-1])
+        print(name)
+        with open(name, "wb") as f_out:
+            f_out.write(response.content)
+    except:
+        pass
